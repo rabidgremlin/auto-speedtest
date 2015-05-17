9f66fc708ba4476b1d407863364ec8ef97e5f9d2
rabidgremlin <jack@rabidgremlin.com>
2015-05-18 11:58:01 +1200

    Morreee

---
+++ cap5min.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+while :
+do
+    ./capture.sh
+    sleep 300
+done
--- capture.sh
+++ capture.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
+
+
+
 NOW=$(date +"%s,%Y-%m-%d %T")
 
 speedtest-cli --server 4135 --simple > /tmp/speedtest-results
+# TODO check for errors
+
+
 CSV_RESULTS=$(awk -vORS=, '{ print $2 }' < /tmp/speedtest-results | sed 's/,$//')
+# TODO check for errors
+
+# TODO check for empty file and create headers
+# ts,datetime,ping,download,upload
 
 echo $NOW,$CSV_RESULTS >> results.csv
 
