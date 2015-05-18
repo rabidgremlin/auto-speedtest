8cef8fab5158f78c73fe6f7ef39c919a92c04313
rabidgremlin <jack@rabidgremlin.com>
2015-05-18 14:10:27 +1200

    Working graph

--- capture.sh
+++ capture.sh
@@ -2,7 +2,6 @@
 
 
 
-
 NOW=$(date +"%s,%Y-%m-%d %T")
 
 speedtest-cli --server 4135 --simple > /tmp/speedtest-results
@@ -16,4 +15,3 @@ CSV_RESULTS=$(awk -vORS=, '{ print $2 }' < /tmp/speedtest-results | sed 's/,$//'
 # ts,datetime,ping,download,upload
 
 echo $NOW,$CSV_RESULTS >> results.csv
-
