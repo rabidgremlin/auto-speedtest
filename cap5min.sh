044e3dca28111c7a0c086f20be1709fc337cd108
Unknown <robkingston0@gmail.com>
2018-03-21 19:28:29 +1100

    found a problem with the delay during some testing

--- cap5min.sh
+++ cap5min.sh
@@ -8,7 +8,8 @@ re='^[0-9]+$'
 if [[ $delay =~ $re ]] ; then #If $delay is not a number (including when it isnt set)
   delay=5
 fi
-time=$(($delay * 60))# $delay is in minutes, $time is in seconds
+sec=60
+time=$((sec*delay))# $delay is in minutes, $time is in seconds
 
 while :
 do
